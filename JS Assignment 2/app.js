d78c54a595e6c1a7180f0c70c933684b136fb417
Muhammad Ashar <124237744+Ashar-1705@users.noreply.github.com>
2023-10-18 12:57:59 +0500

    js assignment 2

---
+++ JS Assignment 2/app.js
@@ -0,0 +1,29 @@
+// CHAPTER # 05
+
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser?
+// var num1 = +prompt("Enter first number:")
+// var num2 = +prompt("Enter second number:")
+// var msg = "sum of both numbers is"
+// document.write(msg + " " + (num1 + num2))
+
+// 2. Repeat task1 for subtraction, multiplication, division & modulus?
+// var num1 = +prompt("Enter first number:")
+// var num2 = +prompt("Enter second number:")
+// var msg = "The subtraction of both numbers is: "
+// document.write(msg + (num1 - num2))
+
+// var msg = "The multiplication of both numbers is: "
+// document.write(msg + (num1 * num2))
+
+// var msg = "The modulus of both numbers is: "
+// document.write(msg + (num1 % num2))
+
+// 3.
+var msg = "Value after variable declaration is: ??"
+document.write(msg)
+var msg = 5
+var eng = "Initial value: "
+document.write(eng + msg)
+var msg = msg++
+var eng = "Value after increment is: "
+document.write(eng + msg)
