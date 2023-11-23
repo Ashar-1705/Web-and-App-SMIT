5e5e450497cf4dc92963ba6968c9c97096774bff
Muhammad Ashar <124237744+Ashar-1705@users.noreply.github.com>
2023-11-23 13:04:15 +0500

    initial commit

---
+++ JS Assignment 3/app.js
@@ -0,0 +1,39 @@
+// CHAPTER 14 - 16
+// Question no: 01
+// ------------------------Solution------------------------
+// let StudentNames = [];
+
+// Question no: 02
+// ------------------------Solution------------------------
+// let StudentNames = {};
+
+// Question no: 03
+// ------------------------Solution------------------------
+// let StringsArray = ["Ashar" , "Waqar" , "Saad"];
+
+// Question no: 04
+// ------------------------Solution------------------------
+// let NumbersArray = [230 , 123 , 43 , 100 , 91];
+
+// Question no: 05
+// ------------------------Solution------------------------
+// let BooleanArray = [true , false , false , true , false];
+
+// Question no: 06
+// ------------------------Solution------------------------
+// let mixArray = ["Ashar" , true , 432 , "Shiza"]
+
+// Question no: 07
+// ------------------------Solution------------------------
+// let qualifications = ["SSC" , "HSC" , "BCS" , "BS" , "BCOM" , "MS" , "M.Phil" , "PhD"];
+// let br = "<br>"
+// console.log("Qualifications")
+// console.log("1)" + " " + qualifications[0])
+// console.log("2)" + " " + qualifications[1])
+// console.log("3)" + " " + qualifications[2])
+// console.log("4)" + " " + qualifications[3])
+// console.log("5)" + " " + qualifications[4])
+// console.log("6)" + " " + qualifications[5])
+// console.log("7)" + " " + qualifications[6])
+// console.log("8)" + " " + qualifications[7])
+
